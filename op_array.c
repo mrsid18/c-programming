c9672c081e6bec7641581d2f3abd2e67d9a95464
mrsid18 <siddharthamehta18@gmail.com>
2017-10-05 14:27:17 +0530

    modified op_array

--- op_array.c
+++ op_array.c
@@ -1,14 +1,18 @@
 #include<stdio.h>
 #include<malloc.h>
 #include<curses.h>
-
+void print();
+void delete();
+void insert();
+void sort(char x);
+int i=0, *head, n;
 void main(){
-	int *p, *head, n;
+	int *p;
 	printf("\nEnter the no. of elements for an array: ");
 	scanf("%d",&n);
 	p=(int *)malloc(sizeof(int));
 	head=p;
-	int i=0;
+	//int i=0;
 	printf("\n\nEnter the elements\n");
 	for(i=0;i<n;i++)
 	{
@@ -18,17 +22,45 @@ void main(){
 
 	printf("\n\nElements of the Array\n\n");
 	printf("\nPos.  Element\n");
-	for(i=0;i<n;i++)
+/*	for(i=0;i<n;i++)
 	{
 		printf("%2d\t %d\n",i+1, *(head+i));
+	}*/
+	print();
+
+	printf("Choose anyone of the following option:\n");
+ch:
+	printf("1.delete an element.\n2.insert an element\n3.sort in ascending order\n4.sort in descending order\n0.Exit\n");
+	int choice;
+	scanf("%d",&choice);
+
+	switch(choice)
+	{
+		case 1: delete();
+			break;
+
+		case 2: insert();
+			break;
+
+		case 3: sort('a');
+			break;
+
+		case 4: sort('d');
+			break;
+
+		case 0: break;
+
+		default: printf("\nIncorrect choice\nPlease enter ur choice again...\n");
+			 goto ch;
+			 
 	}
+	return;
 
-//	printf("Choose anyone of the following option:\n");
-//	printf("1.delete an element.\n2.insert an element\n3.insert an element in between\n4.sort in ascending order\n5.sort in descending order\n0.Exit\n");
-//	int choice;
-//	scanf("%d",&choice);
 
-/*	printf("\nEnter the position of the element to delete: ");
+}
+       void delete()
+       {
+       printf("\nEnter the position of the element to delete: ");
 	int pos;
 	scanf("%d",&pos);
 	for(i=pos-1;i<n-1;i++)
@@ -36,11 +68,18 @@ void main(){
 		*(head+i)=*(head+i+1);
 	}
 	*(head+n-1)=0;
-
-	printf("\n\nprinting the elements\n\n");
-	for(i=0;i<n-1;i++)
+	n--;
+	print();
+	}
+ 
+        void print(){
+	//printf("\n\nprinting the elements\n\n");
+	for(i=0;i<=n-1;i++)
 		printf("%2d\t %d\n",i+1, *(head+i));
 
+	}
+
+/*        void insert(){
 	printf("\nEnter the element to insert at the end: ");
 	int emt;
 	scanf("%d",&emt);
@@ -48,24 +87,26 @@ void main(){
 	n++;
 	printf("\nArray after entering the element.\n\n");
 	for(i=0;i<n;i++)
-		printf("%2d\t %d\n",i+1, *(head+i));
+		printf("%2d\t %d\n",i+1, *(head+i));*/
 
+        void insert(){
         printf("\nEnter the position at which u want to enter the element: ");
-int pos, emt;
-scanf("%d",&pos);
-printf("\nEnter the element: ");
-scanf("%d",&emt);
-for(i=n;i>=pos-1;i--)
-{
+        int pos, emt;
+        scanf("%d",&pos);
+        printf("\nEnter the element: ");
+        scanf("%d",&emt);
+        for(i=n;i>=pos-1;i--)
+        {
 	*(head+i+1)=*(head+i);
-}
-*(head+pos-1)=emt;
-n++;
-for(i=0;i<n;i++)
-printf("%2d\t %d\n",i+1, *(head+i));
-*/
+         }
+          *(head+pos-1)=emt;
+           n++;
+          for(i=0;i<n;i++)
+         printf("%2d\t %d\n",i+1, *(head+i));
+         }
 
-printf("\nSorting Array in ascending order.\n");
+void sort(char x){
+//printf("\nSorting Array in ascending order.\n");
 printf("\nPosition Element\n");
 int j, temp;
 for(i=n-1;i>0;i--)
@@ -81,8 +122,17 @@ for(i=n-1;i>0;i--)
 		}
 	}
 }
-
+if(x=='a')
+{
 for(i=0;i<n;i++)
 printf("%2d\t %d\n",i+1, *(head+i));
-
 }
+
+else
+{
+	for(i=n-1;i>=0;i--)
+		printf("%2d\t %d\n",i+1, *(head+i));
+}
+}
+
+
